3b421a0628c927b995955fb007307f9620f3aea1
Peng <Peng@PENG-PC>
2019-04-30 16:26:12 +0800

    加入專案檔案。

---
+++ Oreilly_Csharp_ch2_P73/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UseTheDebugger
+{
+    /// <summary>
+    /// MainPage.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainPage : Window
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            string name = "Question";
+            int x = 3;
+            x = x * 17;
+            double d = Math.PI / 2;
+            myLabel.Text = "name is " + name
+                + " \nx is " + x
+                + " \nd is " + d;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            int x = 5;
+            if (x == 10)
+            {
+                myLabel.Text = "x must be 10";
+            }
+            else
+            {
+                myLabel.Text = "x isn't 10";
+            }
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            int someValue = 4;
+            string name = "Bobbo Jr.";
+            if ((someValue == 3) && (name == "Joe"))
+            {
+                myLabel.Text = "X is 3 and the name is Joe";
+            }
+            myLabel.Text = "this line runs no matter what";
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+            while (count < 100000000)
+            {
+                count = count + 1;
+            }
+            for (int i = 0; i < 99999923; i++)
+            {
+                count = count - 1;
+            }
+            myLabel.Text = "The answer is " + count;
+        }
+
+
+    }
+}
---
+++ Oreilly_Csharp_ch2_P73/PracticeUsingIfElse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UseTheDebugger
+{
+    public partial class MainPage : Window
+    {
+        private void EnableCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            if (enableCheckbox.IsChecked == true)
+            {
+                if (labelToChange.Text == "Right")
+                {
+                    labelToChange.HorizontalAlignment = HorizontalAlignment.Left;
+                    labelToChange.Text = "Left";
+                }
+                else
+                {
+                    labelToChange.HorizontalAlignment = HorizontalAlignment.Right
+                        ;
+                    labelToChange.Text = "Right";
+                }
+            }
+            else
+            {
+                labelToChange.Text = "";
+                labelToChange.HorizontalAlignment = HorizontalAlignment.Center;
+                labelToChange.Text = "Text Changeing is disabled";
+            }
+        }
+    }
+}
